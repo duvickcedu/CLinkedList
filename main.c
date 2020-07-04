0da558884728785044f00c48f9cb8f9cd9a82d0a
Christian <nr7256bm@go.minnstate.edu>
2020-07-04 14:49:52 -0500

    I have finished the program 	- fixed removeLL() it turned out it was easier than I was making it 	- added documentation

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//Christian Duvick
 
+//structure for a node
 struct Node {
     char letter;
     struct Node *next;
@@ -13,9 +15,13 @@ void insert (char letter);
 int size ();
 void removeLL();
 
+//structures to help with linked list
 struct Node *head = NULL;
 struct Node *tail = NULL;
 
+//controls the interface
+//by default the program exits with the word exit as that's the
+//condition to leave the while loop as long as list is empty
 int main() {
     printf("Would you like to insert, print, remove, or exit?\n>");
     char choice[10];
@@ -48,7 +54,7 @@ int main() {
     return 0;
 }
 
-//prints all nodes. actually works.
+//prints the nodes in order.
 void print (){
     struct Node *currNode = head;
     while (currNode != NULL){
@@ -59,11 +65,13 @@ void print (){
     printf("\n");
 }
 
+//Inserts node in order.
 void insert (char letter){
     struct Node *node = (struct Node*)malloc(sizeof(struct Node));
-    node->letter = letter;
+
     if (head == NULL){
         node->next = tail;
+        node->letter = letter;
         head = node;
     } else {
         struct Node *prevNode = NULL;
@@ -78,15 +86,20 @@ void insert (char letter){
         }
 
         if (head == currNode){
+            node->letter = letter;
             node->next = head;
             head = node;
         } else {
+            node->letter = letter;
             prevNode->next = node;
             node->next = currNode;
         }
+
     }
 }
 
+//used for getting size of the Linked List
+//useful for checking condition to see that list is empty
 int size (){
     int size = 0;
     struct Node *currNode = head;
@@ -97,17 +110,9 @@ int size (){
     return size;
 }
 
+//sets head to null then frees space to delete Linked List
 void removeLL(){
-    int i;
-    struct Node *currNode = head;
-    struct Node *nextNode = NULL;
-    for (i = 0; i < size() - 1; ++i) {
-        nextNode->next = currNode->next;
-        currNode->next = NULL;
-        currNode->letter = NULL;
-        free(currNode);
-        currNode = nextNode;
-    }
-    printf("%d", size());
+    head = NULL;
+    free(head);
     printf("Linked list is now empty\n");
 }
